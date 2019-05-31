4f469b51d342beceab129f3a6c35dee6c4112c2e
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2019-05-30 22:00:09 -0400

    added some print statements

--- core.clj
+++ core.clj
@@ -8,7 +8,7 @@
   (clojure.string/split readline #"\r?\n"))
 
 
-(defn read-in-board [map_file]  
+(defn read-in-board [map_file]
   (str/split-lines (slurp map_file)))
 
 
@@ -24,7 +24,7 @@
       (def down (+ (* y (+ h 1)) w))
       (def right (+ (* y h) (+ w 1)))
       (def left (+ (* y h) (- w 1)))
-      
+
       (cond
         (and (>= (- @i 1) 0) (>= (- w 1) 0)) (aset-int right-left @i 1 left)
         :else (aset-int right-left @i 1 -1))
@@ -37,22 +37,18 @@
       (cond
         (and (< (+ h 1) x) (< (+ @i y) (* x y))) (aset-int top-down @i 1 down)
         :else (aset-int top-down @i 1 -1))
-      
+
       ;;(println @i "-->" top right down (aget right-left @i 1))
       ;;(println @i "-->" (aget top-down @i 0) (aget right-left @i 0) (aget top-down @i 1) (aget right-left @i 1))
       (swap! i inc))))
 
-  
+
 (defn papu [iinit-val, end-val, visited, ppl, xx, yy]
   (aset-int visited iinit-val 0 1)
   (if (= iinit-val end-val)
     (do
       (println "Woo hoo, I found the treasure :-)")
-      
-      (loop [i 0]
-        (when (< i (* xx yy))
-          (println i (aget visited i 0))
-          (recur (inc i))))
+      (println " I can't display the output path because of time constraints ")
       ;(aset-int visited end-val 0 0
       (System/exit 0)))
   ;;(println (aget top-down iinit-val 0) (aget top-down iinit-val 1))
@@ -66,7 +62,8 @@
       (if (and (or (= tempLine2 "@") (= tempLine2 "-")) (= (aget visited t 0) 0))
         (do
           (aset-int ppl t 0 1)
-          (papu t end-val visited ppl xx yy)))))
+          (papu t end-val visited ppl xx yy)
+          (aset-int ppl t 0 1)))))
   (if (>= (aget top-down iinit-val 1) 0)
     (do
       (def t (aget top-down iinit-val 1))
@@ -77,7 +74,8 @@
       (if (and (or (= tempLine2 "@") (= tempLine2 "-")) (= (aget visited t 0) 0))
         (do
           (aset-int ppl t 0 1)
-          (papu t end-val visited ppl xx yy)))))
+          (papu t end-val visited ppl xx yy)
+          (aset-int ppl t 0 1)))))
   (if (>= (aget right-left iinit-val 0) 0)
     (do
       (def t (aget right-left iinit-val 0))
@@ -88,7 +86,9 @@
       (if (and (or (= tempLine2 "@") (= tempLine2 "-")) (= (aget visited t 0) 0))
         (do
           (aset-int ppl t 0 1)
-          (papu t end-val visited ppl xx yy)))))
+          (papu t end-val visited ppl xx yy)
+          (aset-int ppl t 0 1)))))
+
   (if (>= (aget right-left iinit-val 1) 0)
     (do
       (def t (aget right-left iinit-val 1))
@@ -101,14 +101,16 @@
           (aset-int ppl t 0 1)
           (papu t end-val visited ppl xx yy))))))
 
+
 (defn pap [init-val, end-val, xx, yy]
-  
+
   (def visited (make-array Integer/TYPE (* xx yy) 1))
   (def pl (make-array Integer/TYPE (* xx yy) 1))
   (aset-int pl init-val 0 1)
   ;;(println xx yy)
   (papu init-val end-val visited pl xx yy)
-  (println "Uh oh, I could not find the treasure :-("))
+  (println "Uh oh, I could not find the treasure :-(")
+  (println " I can't display the output path because of time constraints "))
 
 (defn main []
   (println "THIS IS TREASURE GAME")
@@ -129,15 +131,15 @@
   (def str1(mapv vec (read-in-board "map.txt")))
   ;;(println str1)
   (def strArr (to-array-2d str1))
-  
-  (doseq  
-    [[value row] (map vector strArr (range))]  
-    (doseq [[value1 column] (map vector value (range))]    
-      (if (= (str (aget strArr row column)) "@")      
-        (def end-row row))      
-      (if (= (str (aget strArr row column)) "@")      
+
+  (doseq
+    [[value row] (map vector strArr (range))]
+    (doseq [[value1 column] (map vector value (range))]
+      (if (= (str (aget strArr row column)) "@")
+        (def end-row row))
+      (if (= (str (aget strArr row column)) "@")
         (def end-col column))))
-  
+
   ;;(println end-row)
   ;;(println end-col)
   (def end-val (+ (* end-row y) end-col))
